78a9fbdc4b3ca55c228ded3996437fa3b0550529
Peter Donis <peterdonis@alum.mit.edu>
2012-09-27 17:07:03 -0400

    Final update for version 0.9.1 release.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ Released under the Python Software Foundation License.
 
 name = "setuputils"
 version = "0.9.1"
-description = "A utility module to automate away boilerplate in Python setup scripts."
+description = "A utility to automate away boilerplate in Python setup scripts."
 startline = 4
 
 author = "Peter A. Donis"
