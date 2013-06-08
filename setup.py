2b30e86ccf1d1a3062fb62fe213339b103a90c05
Peter Donis <peterdonis@alum.mit.edu>
2013-06-08 02:19:00 -0400

    Bumped version number to 0.9.2.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ Released under the Python Software Foundation License.
 
 
 name = "setuputils"
-version = "0.9.1"
+version = "0.9.2"
 description = "A utility to automate away boilerplate in Python setup scripts."
 startline = 4
 
