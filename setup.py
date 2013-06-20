c10bfce94a3a705f6a29dc5a3cc20f70bc35c37f
Peter Donis <peterdonis@alum.mit.edu>
2013-06-20 19:00:31 -0400

    Exclude __pycache__ from search for package data directories; bump version to 0.9.4.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ Released under the Python Software Foundation License.
 
 
 name = "setuputils3"
-version = "0.9.3"
+version = "0.9.4"
 description = "A utility to automate away boilerplate in Python 3 setup scripts."
 startline = 4
 
